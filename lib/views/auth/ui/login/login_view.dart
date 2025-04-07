51bd5b523e1279deffc309bf10f224da31ef7da0
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2025-04-07 03:14:06 +0300

    ..

---
+++ lib/views/auth/ui/login/login_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:nine_aki_bro/views/auth/ui/login/widgets/login_form.dart';
+import 'package:nine_aki_bro/views/auth/ui/login/widgets/login_header.dart';
+import '../../../../common/widgets/login_signup/form_divider.dart';
+import '../../../../common/widgets/login_signup/social_button.dart';
+import '../../../../core/constants/sizes.dart';
+import '../../../../core/constants/text_string.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              /// Logo , Title & Sub-Title
+              TLoginHeader(),
+
+              /// Form
+              TLoginForm(),
+
+              /// Divider
+              TFormDivider(
+                dividerText: TTexts.orSignInWith,
+              ),
+              SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+
+              /// Footer
+              TSocialButtons(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
